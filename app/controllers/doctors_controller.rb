9c70f68c3f5fcfcc16857cc2c54391d26ac253d9
Anthony Furno <amfurno17@gmail.com>
2026-04-08 00:15:17 +0000

    finish new doctor form

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -10,4 +10,33 @@ class DoctorsController < ApplicationController
   def new
     @doctor = Doctor.new
   end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+    if @doctor.save
+      redirect_to @doctor
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    if @doctor.update(doctor_params)
+      redirect_to @doctor
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def doctor_params
+    params.expect(doctor: [ :name, :practice, :speciality, :email, :phone_number, :fax_number, :address ])
+  end
+
 end
