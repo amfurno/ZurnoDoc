702e263c880205bb3a672efb83381dc729e14e13
Anthony Furno <amfurno17@gmail.com>
2026-05-27 03:48:16 +0000

    add nav bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
+  after_action :persist_current_patient, if: -> { @patient.present? }
 
   rescue_from Pundit::NotAuthorizedError, with: :pundit_not_authorized
 
@@ -27,5 +28,20 @@ class ApplicationController < ActionController::Base
 
   def pundit_user
     Current.user
+  end
+
+  def current_patient
+    return unless authenticated? && session[:current_patient_id].present?
+
+    @current_patient ||= begin
+      patient = Current.user.patients.find_by(id: session[:current_patient_id])
+      session[:current_patient_id] = nil unless patient
+      patient
+    end
+  end
+  helper_method :current_patient
+
+  def persist_current_patient
+    session[:current_patient_id] = @patient.id
   end
 end
